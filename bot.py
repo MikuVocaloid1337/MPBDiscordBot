0356f1e2469eab63adcc1c3601c68881b2ab5b22
MikuVocaloid1337 <96827003+MikuVocaloid1337@users.noreply.github.com>
2025-02-20 19:01:37 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+
+import requests
+import a2s
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1342146792921497712/rJVBkbox_QV2b4JaSAGDYtRioci2905SiPhFXgI-2pc-eigODkcTEZ-Jhpt0G0niR1fP"
+SERVER_IP = "62.122.215.43"
+SERVER_PORT = 27047
+
+try:
+    server_address = (SERVER_IP, SERVER_PORT)
+    info = a2s.info(server_address)
+    players = a2s.players(server_address)
+    status = f"🎮 **{info.server_name}**\n" \
+             f"📡 **IP:** {SERVER_IP}:{SERVER_PORT}\n" \
+             f"👥 **Игроки:** {len(players)}/{info.max_players}\n" \
+             f"🎭 **Карта:** {info.map_name}"
+except:
+    status = "❌ Сервер недоступен!"
+
+requests.post(WEBHOOK_URL, json={"content": status})
+
